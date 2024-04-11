7d8d04953948a79d26e1f63e16932a0ef66ebe1a
athomft <atho.mft@gmail.com>
2024-04-11 16:05:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Set the date we're counting down to
+const countDownDate = new Date("May 31, 2024 00:00:00").getTime();
+
+// Update the countdown every 1 second
+const x = setInterval(function () {
+
+    // Get the current date and time
+    const now = new Date().getTime();
+
+    // Calculate the distance between now and the countdown date
+    const distance = countDownDate - now;
+
+    // Calculate days, hours, minutes, and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the countdown
+    document.getElementById("countdown").innerHTML = "Estimated time remaining: " + days + "d " +
+        hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    // If the countdown is over, display a message
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "The website is live now!";
+    }
+}, 1000);
